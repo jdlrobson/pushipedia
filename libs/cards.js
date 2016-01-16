a4fc352f5b80f227cf1f052592603be3861b9b46
jdlrobson <jdlrobson@gmail.com>
2016-01-16 09:42:35 -0800

    Cache cards request
    Currently generation of a card hits the MediaWiki API on every request.
    Instead of doing this let's cache for 20 minutes.

--- libs/cards.js
+++ libs/cards.js
@@ -1,23 +1,21 @@
 var fetch = require('node-fetch');
-function respondWithJsonCard( resp, pageTitle, project, additionalData ) {
-	var base = 'https://en.wikipedia.org';
-	var qs = 'action=query&prop=pageimages|extracts&piprop=thumbnail&format=json&formatversion=2&explaintext=&titles=' + encodeURIComponent( pageTitle );
+var NodeCache = require( "node-cache" );
+var myCache = new NodeCache( { stdTTL: 60 * 20, checkperiod: 60 * 5 } );
 
+function respondWithJsonCard( resp, pageTitle, project, additionalData ) {
+	var fullUrl,
+		base = 'https://en.wikipedia.org',
+		qs = 'action=query&prop=pageimages|extracts&piprop=thumbnail&format=json&formatversion=2&explaintext=&titles=' + encodeURIComponent( pageTitle );
 
 	if ( project === 'commons' ) {
 		base = 'https://commons.wikimedia.org';
 	}
 	additionalData = additionalData || {};
 
-	fetch( base + '/w/api.php?' + qs ).then( function ( wikiResp ) {
-		if (wikiResp.status !== 200) {
-			resp.status( 503 );
-		}
-		wikiResp.json().then( function ( data ) {
-			var page,
-				pages = data.query.pages;
-			if ( pages.length ) {
-				page = pages[0];
+	fullUrl = base + '/w/api.php?' + qs;
+	myCache.get( fullUrl, function( err, page ) {
+		if ( err || page === undefined ) {
+			getCardFromServer( fullUrl ).then( function ( page ) {
 				// assign all values in data
 				for( key in additionalData ) {
 					if ( additionalData.hasOwnProperty( key ) ) {
@@ -26,9 +24,35 @@ function respondWithJsonCard( resp, pageTitle, project, additionalData ) {
 				}
 				resp.setHeader('Content-Type', 'application/json');
 				resp.send( page );
-			} else {
-				resp.status( 500 );
+			}, function () {
+				resp.status( 503 );
+				resp.send( 'nope' );
+			} );
+		} else {
+			console.log( 'load from cache', page );
+			resp.setHeader('Content-Type', 'application/json');
+			resp.send( page );
+		}
+	} );
+}
+
+function getCardFromServer( fullUrl ) {
+	return new Promise( function ( resolve, reject ) {
+		fetch( fullUrl ).then( function ( wikiResp ) {
+			if (wikiResp.status !== 200) {
+				reject();
 			}
+			wikiResp.json().then( function ( data ) {
+				var page,
+					pages = data.query.pages;
+				if ( pages.length ) {
+					page = pages[0];
+					myCache.set( fullUrl, page );
+					resolve( page );
+				} else {
+					reject();
+				}
+			} );
 		} );
 	} );
 }
